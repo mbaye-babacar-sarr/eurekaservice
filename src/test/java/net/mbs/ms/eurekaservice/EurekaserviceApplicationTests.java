151aea0f624e0bc9aba59ffea5e787a830d70b7c
mbsdev <mbsdev2018@gmail.com>
2020-07-19 14:19:30 +0000

    init eureka serveur

---
+++ src/test/java/net/mbs/ms/eurekaservice/EurekaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mbs.ms.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
